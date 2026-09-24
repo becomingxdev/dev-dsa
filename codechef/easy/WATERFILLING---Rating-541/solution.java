0606e985c5c8ff98a274922d11a25fd9a850bddc
Dev Desai <desai.dev742@gmail.com>
2026-09-24 09:16:27 +0530

    ✅ WATERFILLING - Rating 541 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-09-24

---
+++ codechef/easy/WATERFILLING---Rating-541/solution.java
@@ -0,0 +1,41 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		
+		int t = sc.nextInt();
+		
+		for(int i = 0; i < t; i++) {
+		    
+		    int count = 0;
+		    
+		    for(int j = 0; j < 3; j++) {
+		        
+		        int x = sc.nextInt();
+		        
+		        if(x == 0) {
+		            count++;
+		        } else {
+		            count--;
+		        }
+		        
+		    }
+		    
+		    if(count >= 1) {
+		        System.out.println("Water filling time");
+		    } else {
+		        System.out.println("Not now");
+		    }
+		    
+		}
+		
+		sc.close();
+
+	}
+}
